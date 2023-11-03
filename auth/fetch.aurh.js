2c3ceb7d42aae52a209cba73bb429b87cf80fecb
vKurama7u7v <aurelio.marin@iest.edu.mx>
2023-11-02 21:45:10 -0600

    [feat]: add fetch.auth.js file :pushpin:

---
+++ auth/fetch.aurh.js
@@ -0,0 +1,34 @@
+import { getToken, hasExpiredToken } from "./tokens.auth";
+
+export async function authFetch(url, params, logout) {
+  const token = getToken();
+
+  if (!token) {
+    // Usuario no logueado
+    logout();
+  } else {
+    // Token valido
+    if (hasExpiredToken(token)) {
+      // Token Caducado (return true)
+      logout();
+    } else {
+      // Token valido
+      const paramsTemp = {
+        ...params,
+        headers: {
+          ...params?.headers,
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const response = await fetch(url, paramsTemp);
+        const result = await response.json();
+        return result;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    }
+  }
+}
